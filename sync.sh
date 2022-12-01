d0cca8a5f2644eebe7c4e9a19fd3f4cfd0949a5e
Lijun Yu <lijun@lj-y.com>
2022-11-30 20:18:18 -0500

    add acknowledgements

---
+++ sync.sh
@@ -0,0 +1,4 @@
+cp -r _* ../magvit-ann.github.io/
+cd ../magvit-ann.github.io
+find . -type f | grep html | xargs -I {} sed -i '' -e '1h;2,$H;$!d;g' -e 's/<!--strip anonymous begin-->.*<!--strip anonymous end-->//g' {}
+find . -type f | grep yml | xargs -I {} sed -i '' -e '1h;2,$H;$!d;g' -e 's/<!--strip anonymous begin-->.*<!--strip anonymous end-->//g' {}
