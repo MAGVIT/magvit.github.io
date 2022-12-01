14c5f165d4a42174d7565e039d3001909214c760
Lijun Yu <lijun@lj-y.com>
2022-11-30 20:49:41 -0500

    add paper

--- sync.sh
+++ sync.sh
@@ -1,4 +1,7 @@
-cp -r _* ../magvit-ann.github.io/
+cp -r _* img ../magvit-ann.github.io/
 cd ../magvit-ann.github.io
 find . -type f | grep html | xargs -I {} sed -i '' -e '1h;2,$H;$!d;g' -e 's/<!--strip anonymous begin-->.*<!--strip anonymous end-->//g' {}
 find . -type f | grep yml | xargs -I {} sed -i '' -e '1h;2,$H;$!d;g' -e 's/<!--strip anonymous begin-->.*<!--strip anonymous end-->//g' {}
+git add .
+git commit -m "Synced changes"
+git push
